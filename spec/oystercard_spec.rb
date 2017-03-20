49d22efacf06d7b32bdd21ac80e7f8bc53018f40
alicespyglass <alicespyglass@gmail.com>
2017-03-20 17:33:30 +0000

    completed ex5 - created top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,18 @@ require 'oystercard'
 describe Oystercard do
   subject(:oystercard) { described_class.new }
 
-  it 'has an initial balance of zero' do
-    expect(oystercard.balance).to eq(0)
+  describe '#initialze' do
+    it 'has an initial balance of zero' do
+      expect(oystercard.balance).to eq(0)
+    end
   end
+
+  describe '#top_up' do
+    it {is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'adds top-up value to the balance' do
+      expect{ oystercard.top_up 20}.to change{ oystercard.balance }.by 20
+    end
+  end
+
 end
