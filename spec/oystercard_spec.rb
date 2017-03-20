f441174c8b8d68009b675366647682c7bd6d389c
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-03-20 17:09:29 +0000

    completed ex4, adding initial balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
 
+  it 'has an initial balance of zero' do
+    expect(oystercard.balance).to eq(0)
+  end
 end
