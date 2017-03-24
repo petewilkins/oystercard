5ac0c061a3c19795b9fdcaf6537f8c8e133f6b5a
Stephanie Crampin <steph.crampin@gmail.com>
2017-03-24 10:04:32 +0000

    added failing test for start method

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,3 +1,9 @@
 class JourneyLog
 
+  attr_reader :journey_class
+
+  def initialize(inputclass = Journey)
+    @journey_class = inputclass
+  end
+
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,10 +2,20 @@ require 'journey_log'
 
 describe JourneyLog do
   subject(:journeylog) { described_class.new }
+  let(:station) {double :station}
+
 
   describe '#initialze' do
     it 'sets to Journey by default' do
       expect(journeylog.journey_class).to eq Journey
     end
   end
+
+  describe '#start' do
+    it 'creates a new journey' do
+      expect(journeylog.journey_class).to receive(:new)
+      journeylog.start(station)
+    end
+  end
+
 end
